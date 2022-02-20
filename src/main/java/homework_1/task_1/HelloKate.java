f9cf74f11f5b9a16c14cfc979b41176f5b0b0fbe
Katrin <Karkusha869@gmail.com>
2022-02-20 23:18:52 +0300

    initial commit

--- src/main/java/homework_1/task_1/HelloKate.java
+++ src/main/java/homework_1/task_1/HelloKate.java
@@ -1,2 +1,9 @@
-package homework_1.task_1;public class HelloKate {
+package homework_1.task_1;
+
+public class HelloKate{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello," +args[0]);
+    }
 }
