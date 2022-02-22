1cedc50e7ee5afcecb54e185abb050846805a7d0
Katrin <Karkusha869@gmail.com>
2022-02-22 23:05:08 +0300

    commit

--- src/main/java/homework_2/task_1/RunningCycle.java
+++ src/main/java/homework_2/task_1/RunningCycle.java
@@ -1,2 +1,22 @@
-package homework_2.task_1;public class RunningCycle {
+package homework_2.task_1;
+
+import static java.lang.System.out;
+
+public class RunningCycle {
+    public static void main(String[] args) {
+
+        int t=0;
+        int a=1;
+
+        do {
+            System.out.println("Прошло " + t + " часов, стало" +a +" амеб");
+            t = t + 3;
+            a=a*2;
+        }
+        while(t<=24);
+
+
+
+    }
+
 }
--- src/main/java/homework_2/task_2/DuplicationAmoebas.java
+++ src/main/java/homework_2/task_2/DuplicationAmoebas.java
@@ -1,2 +1,20 @@
-package homework_2.task_2;public class DuplicationAmoebas {
+package homework_2.task_2;
+
+public class DuplicationAmoebas {
+    public static void main(String[] args) {
+
+        int t = 0; // время
+        int a = 1; // амебы
+
+        do {
+            System.out.println("Прошло " + t + " часа(-ов), стало " + a + " амеб(-ы)");
+            t = t + 3;
+            a = a * 2;
+        }
+        while (t <= 24);
+
+
+    }
+
 }
+
