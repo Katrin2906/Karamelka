5d42c492c98ff4012efb171fe2ae0f8eb38d4ab0
Katrin <Karkusha869@gmail.com>
2022-02-23 23:47:41 +0300

    initial commit

--- src/main/java/homework_2/task_5/TableInches.java
+++ src/main/java/homework_2/task_5/TableInches.java
@@ -2,6 +2,14 @@ package homework_2.task_5;
 
 public class TableInches {
     public static void main(String[] args) {
+        System.out.println("Дюймы, in" + " | " + " сантиметры, см ");
+
+        for (int i = 1; i <= 20; i = i + 1) {
+            double a = 1;
+            a = i * 2.54;
+            System.out.println(i + " | " + a);
+        }
+
 
     }
 }
