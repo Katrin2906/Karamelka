589871e3763008ff14a972977bf4530867f5ad15
Katrin <Karkusha869@gmail.com>
2022-02-23 23:59:52 +0300

    initial commit

---
+++ src/main/java/homework_2/task_6/EvenNumber.java
@@ -0,0 +1,9 @@
+package homework_2.task_6;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i = i + 2) {
+            System.out.print(i + " | ");
+        }
+    }
+}
