d5e68ef3c94cb6e251f4852385ae52eafcc365af
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 19:05:02 +0100

    Update EvenNumber.java

--- src/main/java/homework_2/task_6/EvenNumber.java
+++ src/main/java/homework_2/task_6/EvenNumber.java
@@ -2,6 +2,7 @@ package homework_2.task_6;
 
 public class EvenNumber {
     public static void main(String[] args) {
+        // твоя программа работает, но не совсем корректна. Лучше добавить if() с проверкой на четность, а не шагать even+2
         for (int even = 2; even <= 100; even = even + 2) {
             System.out.print(even + " | ");
         }
