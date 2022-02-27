3913843be5c15684544b9543c046758a6831a096
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 19:14:28 +0100

    Update DifferentSums.java

--- src/main/java/homework_2/task_8/DifferentSums.java
+++ src/main/java/homework_2/task_8/DifferentSums.java
@@ -1,8 +1,10 @@
 package homework_2.task_8;
 
 public class DifferentSums {
+    // ты решила задачу при помощи двух циклов. Подумай как решить ее при помощи только одного, т.к. такая программа будет работать гораздо быстрее.
+    // подсказка в комментарии к задаче 6,7
     public static void main(String[] args) {
-        int sum_even = 0;
+        int sum_even = 0; // название переменной sum_even -> sumEven
         for (int even = 0; even <= 100; even = even + 2) {
             sum_even = sum_even + even;
 
