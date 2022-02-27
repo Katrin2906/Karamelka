044f875c629d525a491b5e3b42cad33b9fc24165
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 19:03:01 +0100

    Update TableInches.java

--- src/main/java/homework_2/task_5/TableInches.java
+++ src/main/java/homework_2/task_5/TableInches.java
@@ -5,11 +5,11 @@ public class TableInches {
         System.out.println("Дюймы, in" + " | " + " сантиметры, см ");
 
         for (int inches = 1; inches <= 20; inches = inches + 1) {
-            double sm = 1;
+            double sm = 1; // создание переменной стоит вынести за пределы цикла. Зачем ее каждый раз создавать заново?
             sm = inches * 2.54;
             System.out.println(inches + " | " + sm);
         }
-
+// 12,13 строки можно удалить.
 
     }
 }
