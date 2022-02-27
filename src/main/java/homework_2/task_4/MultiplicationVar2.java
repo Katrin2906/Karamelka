09108b21129723f7d54f023d7d5004b8458f24bd
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 19:00:11 +0100

    Update MultiplicationVar2.java

--- src/main/java/homework_2/task_4/MultiplicationVar2.java
+++ src/main/java/homework_2/task_4/MultiplicationVar2.java
@@ -1,6 +1,7 @@
 package homework_2.task_4;
 
 public class MultiplicationVar2 {
+    // этот код не компилируется
     public static void main(String[] args) {
         int n = 0;
         int a = a;
