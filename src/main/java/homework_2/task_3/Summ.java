577330d57b077a2e6b7c1b50a330dc1b326a1ae1
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 18:58:55 +0100

    Update Summ.java

--- src/main/java/homework_2/task_3/Summ.java
+++ src/main/java/homework_2/task_3/Summ.java
@@ -2,7 +2,7 @@ package homework_2.task_3;
 
 public class Summ {
     public static void main(String[] args) {
-        int i = 1;
+        int i = 1; // если очень хочется использовать i как название переменной, то тогда лучше цикл while заменить на for
         int sum = 0;
         while (i <= 256) {
             sum = sum + i;
