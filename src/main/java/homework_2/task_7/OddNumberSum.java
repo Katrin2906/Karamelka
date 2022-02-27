95cd2cf6b0a0de89069365577ae416b788d867ce
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-27 19:05:58 +0100

    Update OddNumberSum.java

--- src/main/java/homework_2/task_7/OddNumberSum.java
+++ src/main/java/homework_2/task_7/OddNumberSum.java
@@ -3,6 +3,7 @@ package homework_2.task_7;
 public class OddNumberSum {
     public static void main(String[] args) {
         int sum = 0;
+        // твоя программа работает, но не совсем корректна. Лучше добавить if() с проверкой на нечетность, а не шагать odd + 2
         for (int odd = 1; odd <= 99; odd = odd + 2) {
             sum = sum + odd;
         }
