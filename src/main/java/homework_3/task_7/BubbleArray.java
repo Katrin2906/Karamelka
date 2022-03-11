38bec93339fcf33314eb11f961466cd27a8bb754
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-11 08:04:06 +0100

    Update BubbleArray.java

--- src/main/java/homework_3/task_7/BubbleArray.java
+++ src/main/java/homework_3/task_7/BubbleArray.java
@@ -7,7 +7,7 @@ public class BubbleArray {
 
         int[] bubbleArray = {4, 15, 178, -23, 0, 0, 1000};
         boolean sorted = false;
-        int sort;
+        int sort; // sort - больше похоже на переменную типа boolean, temp/tmp/replacement и т.д. здесь больше подойдет
         while (!sorted) {
             sorted = true;
             for (int i = 0; i < bubbleArray.length - 1; i++) {
