94af4d35e3ba6866dc67c219fea5fb92badea85f
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-11 07:50:20 +0100

    Update ArithmeticMean.java

--- src/main/java/homework_3/task_2/ArithmeticMean.java
+++ src/main/java/homework_3/task_2/ArithmeticMean.java
@@ -9,10 +9,10 @@ public class ArithmeticMean {
         double mean = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array length ");
-        int х = scanner.nextInt();
+        int х = scanner.nextInt(); // x - это длина массива, вот и назови переменную arrayLength
         int[] arrayMean = new int[х];
 
-        for (int i = 0; i < arrayMean.length; i=++i) {
+        for (int i = 0; i < arrayMean.length; i=++i) { // i = ++i - это некорректная запись, просто i++
             arrayMean[i] = i;
             summ = summ + arrayMean[i];
         }
