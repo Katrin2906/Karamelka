83a761f6cfeca823a16e875ac9f08797e10a5b75
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-11 07:52:56 +0100

    Update OddArray.java

--- src/main/java/homework_3/task_3/OddArray.java
+++ src/main/java/homework_3/task_3/OddArray.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 
 public class OddArray {
     public static void main(String[] args) {
+        // а зачем оставила столько законмментированного кода? если не нужен, то удаляй
         //Scanner scanner = new Scanner(System.in);
         // System.out.println("Enter array length ");
         //int х = scanner.nextInt();
         //int[] oddArray = new int[х];
-        Random r = new Random();
-        int x = r.nextInt(50);
+        Random r = new Random(); // называй переменные согласно тому, что они в себе хранят. В данном случае r -> random
+        int x = r.nextInt(50); // x -> randomInt
         int[] oddArray = new int[x];
 
         for (int i = 0; i < oddArray.length; ++i) {
