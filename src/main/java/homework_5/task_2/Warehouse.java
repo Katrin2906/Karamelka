7a20df74a56257958603a2711965b0a3b9d490b5
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-18 08:38:15 +0100

    Update Warehouse.java

--- src/main/java/homework_5/task_2/Warehouse.java
+++ src/main/java/homework_5/task_2/Warehouse.java
@@ -1,12 +1,13 @@
 package homework_5.task_2;
 
 // код надо отформатировать
+// я бы рекомендовал добавить метод main для проверки
 public class Warehouse {
   private Integer jawerly [];
   private Integer index;
 
    public Warehouse(Integer[] jawerly) {
-      this.jawerly = jawerly;
+      this.jawerly = jawerly; // вот здесь проблема, при помощи метода getArray() я могу получить массив и присвоить новое значение любой ячейке
       index= jawerly.length-1;
    }
 
