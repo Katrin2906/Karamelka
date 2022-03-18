3cd64456e198eb11befce200c2a4bca93fe58342
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-18 08:39:26 +0100

    Update HouseList.java

--- src/main/java/homework_5/task_3/HouseList.java
+++ src/main/java/homework_5/task_3/HouseList.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class HouseList {
     public static void main(String[] args) {
-        Object[] house = new Object[4];
+        Object[] house = new Object[4]; // house -> houses, там же несколько домов
 
         house[0] = new CityHouse(34, 118, true);
         house[1] = new TownHouse(2, 6, false);
