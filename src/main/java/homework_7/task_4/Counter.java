275254664867a60668f8f8b4262794dbd206e3dc
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-25 08:25:14 +0100

    Update Counter.java

--- src/main/java/homework_7/task_4/Counter.java
+++ src/main/java/homework_7/task_4/Counter.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Counter {
     private String otherText;
+    // ты назвала поле MAIN_TEXT, т.е. все большими буквами, такие поля должны быть объявлены как static final и идти первыми в классе
     private String MAIN_TEXT = "While major gameplay components are already in place and functioning,\n" +
             "players help is needed to add some of the smaller features and content.\n" +
             "Of course polishing the overall experience is important at the same time.\n" +
@@ -12,6 +13,8 @@ public class Counter {
             "Plan is to make the game as good as possible but without compromising the core idea";
     private Integer counter;
 
+    // молодец что попробовала сделать это в форме классе, но здесь было бы достаточно класса с методом main + 1 приватный метод,
+    // с аргументом в виде виде строки для поиска или двумя аргументами String что_искать, String где_искать
     public Counter(String otherText) {
         this.otherText = otherText;
     }
