1df96b36596d239c29f44bf778c7b84abf4ce438
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-25 08:19:21 +0100

    Update StringReverseOther.java

--- src/main/java/homework_7/task_2/StringReverseOther.java
+++ src/main/java/homework_7/task_2/StringReverseOther.java
@@ -4,12 +4,12 @@ public class StringReverseOther {
 
     public static String reverseString(String reverse) {
         String result = "";
-        for (int i = 0; i < reverse.length(); i++) {
+        for (int i = 0; i < reverse.length(); i++) { // а не проще было бы бежать с конца строки?
             result = reverse.charAt(i) + result;
         }
         return result;
-    }
-    public static void main(String[] args) {
+    } // абзац
+    public static void main(String[] args) { // метод main должен идти первым в классе
         System.out.println(reverseString("alucard"));
     }
 }
