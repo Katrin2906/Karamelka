51856b9a05723e907fa8c61bb9b9527eadbbcb2b
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-29 19:20:03 +0200

    Update ArrayLengthMain.java

--- src/main/java/homework_8/task_3/ArrayLengthMain.java
+++ src/main/java/homework_8/task_3/ArrayLengthMain.java
@@ -3,6 +3,8 @@ package homework_8.task_3;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+// твоей программе явно не хватает цикла. Т.е. если пользователь ввел некорректное число, то программа еще раз просит ввести число, а не завершает работу
+// сделай while(true) с выходом через break или какой-нибудь флаг boolean askUserAgain, который поменяешь на false когда пользователь введет корректные данные
 public class ArrayLengthMain {
     public static void main(String[] args) throws ArrayExeption {
 
@@ -18,7 +20,7 @@ public class ArrayLengthMain {
         } catch (InputMismatchException e) {
             System.out.println("Incorrect format, try again");
         } catch (ArrayExeption ex) {
-            System.out.println("Incorrect number.Try again.");
+            System.out.println("Incorrect number.Try again."); // явно не хватает информации. Скажи пользователю про длинну массива и положительные числа
         }
     }
 }
