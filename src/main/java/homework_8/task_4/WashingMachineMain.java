8b9bb6021fad12ed4b8506db6516e960b78dfec5
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-29 19:27:20 +0200

    Update WashingMachineMain.java

--- src/main/java/homework_8/task_4/WashingMachineMain.java
+++ src/main/java/homework_8/task_4/WashingMachineMain.java
@@ -4,12 +4,12 @@ public class WashingMachineMain {
     public static void main(String[] args) throws ElectricityExeption {
 
         try {
-            WashingMachine little = new WashingMachine();
+            WashingMachine little = new WashingMachine(); // вынеси WashingMachine little выше блока try, тогда ты сможешь в блоке catch вызвать метод waterOff()
             little.workingMachine(true);
             System.out.println(little.workingMachine(true));
 
         } catch (ElectricityExeption e) {
-            WashingMachine little = new WashingMachine();
+            WashingMachine little = new WashingMachine(); // таким образом ты слила воду у совершенно другой машинки. Ты ведь создала новый объект класса WashingMachine
             System.out.println(little.waterOff());
             System.out.println("No electricity, water out");
         }
