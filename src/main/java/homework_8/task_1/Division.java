a64b96948078f588d45e242ee8a9f0d2cdbd1237
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-29 19:09:53 +0200

    Update Division.java

--- src/main/java/homework_8/task_1/Division.java
+++ src/main/java/homework_8/task_1/Division.java
@@ -2,7 +2,7 @@ package homework_8.task_1;
 
 public class Division {
 
-    public static Integer Division(Integer firstNum, Integer secondNum) {
+    public static Integer Division(Integer firstNum, Integer secondNum) { // названия методов с маленькой буквы Division -> division
         return firstNum / secondNum;
     }
 }
