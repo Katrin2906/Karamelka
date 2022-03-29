58c4b2fa8578cd3e44e48a36164836e656435e4e
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-29 19:11:12 +0200

    Update DivisionMain.java

--- src/main/java/homework_8/task_1/DivisionMain.java
+++ src/main/java/homework_8/task_1/DivisionMain.java
@@ -2,10 +2,10 @@ package homework_8.task_1;
 
 public class DivisionMain {
     public static void main(String[] args) {
-
+// лишняя строка
         try {
             Division.Division(15, 0);
-        } catch (Exception e) {
+        } catch (Exception e) { // Exception замени на ArithmeticException
             System.out.println("На 0 в Java целые числа не делят");
         }
     }
