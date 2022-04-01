800968c0a3edf326192c32a52fb006537ff3dd55
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:47:11 +0200

    Update List.java

--- src/main/java/homework_9/task_7/List.java
+++ src/main/java/homework_9/task_7/List.java
@@ -2,12 +2,13 @@ package homework_9.task_7;
 
 import java.io.File;
 
+// название класса List не очень хорошо отображает его предназначение. Что-нибудь вроде FileSystem было бы гораздо понятнее
 public class List {
 
-    public void search(String path) {
+    public void search(String path) { // думаю лучше в метод передавать File, а не String, тогда 9 строка не нужна будет
         File list = new File(path);
         File[] newList = list.listFiles();
-        if (newList == null) return;
+        if (newList == null) return; // всегда используй скобки {} для блока if и др
         for (File roots : list.listFiles()) {
             if (roots.isDirectory()) {
                 search(roots.getPath());
