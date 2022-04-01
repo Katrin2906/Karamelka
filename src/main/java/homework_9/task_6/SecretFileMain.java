3a9e6c4daea01b04ea8ff16b82ba0c7ecd05ae43
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:40:21 +0200

    Update SecretFileMain.java

--- src/main/java/homework_9/task_6/SecretFileMain.java
+++ src/main/java/homework_9/task_6/SecretFileMain.java
@@ -2,6 +2,7 @@ package homework_9.task_6;
 
 import java.awt.image.BufferedImage;
 
+// код не отформатирован
 public class SecretFileMain {
     public static void main(String[] args) {
         BufferedImage bufferedImage=new BufferedImage(45,45,1);
