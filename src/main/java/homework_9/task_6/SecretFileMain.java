55a16a42e672f4f58a75d237bace198c7814490d
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:42:10 +0200

    Update SecretFileMain.java

--- src/main/java/homework_9/task_6/SecretFileMain.java
+++ src/main/java/homework_9/task_6/SecretFileMain.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 
 // код не отформатирован
 public class SecretFileMain {
+    // задачей было записать сообщение в любой файл с картинкой при помощи append и затем восстановить это сообщение. Решение можешь посмотреть у Саши Козлова
     public static void main(String[] args) {
         BufferedImage bufferedImage=new BufferedImage(45,45,1);
         System.out.println(bufferedImage);
