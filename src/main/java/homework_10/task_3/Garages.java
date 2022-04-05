461102d8cdac56c849cf23e0fe8226a401fbdb8d
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-05 08:05:42 +0200

    Update Garages.java

--- src/main/java/homework_10/task_3/Garages.java
+++ src/main/java/homework_10/task_3/Garages.java
@@ -4,8 +4,8 @@ package homework_10.task_3;
 public class Garages<T> {
 
     private T auto;
-    private T BMW;
-    private T Lada;
+    private T BMW; // BMW -> bmw
+    private T Lada; // Lada -> lada - это не русский язык, где названия нужно писать с большой буквы
     private T name;
 
     Garages(T name) {
@@ -24,16 +24,35 @@ public class Garages<T> {
         return Lada;
     }
 
+    // у тебя метод называется compareBMW а выглядит как equals. Compare(сравнение) это про другое
     public String compareBMW(Garages auto, Garages auto1) {
         if (!(auto.getAuto().equals(getBMW()))) {
             return "В общий";
         } else return " ВMW";
     }
 
+    // у тебя метод называется compareLada а выглядит как equals. Compare(сравнение) это про другое
     public String compareLada(Garages auto, Garages auto1) {
         if (!(auto.getAuto().equals(getLada()))) {
             return "В общий";
         } else return " Lada";
+    }
+    
+    public static void main(String[] args) {
+        Bmw[] bmws = {new Bmw("X5")};
+
+        Garage<Bmw> garage = new Garage<>(bmws);
+
+        garage.cars()[0] = new Bmw("X3");
+        /*
+        garage.cars()[0] = new Lada("Granta"); - этот код не будет компилироваться, т.к. в твоем гараже генериком указаны Bmw
+        
+        чтобы сделать гараж с Lada надо сделать так
+        
+        Bmw[] ladas = {new Lada("Granta")};
+
+        Garage<Lada> ladaGarage = new Garage<>(ladas);
+        */
     }
 
 }
