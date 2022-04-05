a9659400dcef94c2cec7a56e619b2fbd76f59cb7
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-05 07:51:59 +0200

    Update StrategyMain.java

--- src/main/java/homework_10/task_2/StrategyMain.java
+++ src/main/java/homework_10/task_2/StrategyMain.java
@@ -6,10 +6,15 @@ public class StrategyMain {
         String[] name = {"Tjknj98hj", "HgtTTTjjn", "O_JINjy"};
 
         for (int i = 0; i < name.length; i++) {
+            // file - это не файл, а стратегия по его переименованию, файл у тебя это name[i]
             Strategy file = Strategy.LOWERCASE;
             System.out.println(file.rename(name[i]));
+            
+            // file1 - это не файл, а стратегия по его переименованию, файл у тебя это name[i]
             Strategy file1 = Strategy.UPPERCASE;
             System.out.println(file1.rename(name[i]));
+            
+            // file2 - это не файл, а стратегия по его переименованию, файл у тебя это name[i]
             Strategy file2 = Strategy.UPPERFIRST;
             System.out.println(file2.rename(name[i]));
         }
