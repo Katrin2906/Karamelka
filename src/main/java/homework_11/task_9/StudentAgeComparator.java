a2f4db9f23d95f2cd3d30fd4833bdbe394adfe59
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-12 08:46:25 +0200

    Update StudentAgeComparator.java

--- src/main/java/homework_11/task_9/StudentAgeComparator.java
+++ src/main/java/homework_11/task_9/StudentAgeComparator.java
@@ -2,6 +2,7 @@ package homework_11.task_9;
 
 import java.util.Comparator;
 
+// +
 public class StudentAgeComparator implements Comparator<Student> {
     @Override
     public int compare(Student o1, Student o2) {
