6c827b0dc8c49391f0d7d69762cf7d702b9f9cd7
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-12 07:45:49 +0200

    Update Numbers.java

--- src/main/java/homework_11/task_2/Numbers.java
+++ src/main/java/homework_11/task_2/Numbers.java
@@ -13,12 +13,25 @@ public class Numbers {
         String text = String.format("Initial list %s", numbers);
         System.out.println(text);
 
+        // этот подход неверный, т.к. ты в каждой новой итерации создаешь новый iterator, а этого делать не нужно
         for (Iterator<Integer> iterator = numbers.iterator(); iterator.hasNext(); ) {
             Integer number = iterator.next();
             if (number % 2 == 0) {
                 iterator.remove();
             }
         }
+        
+        /*
+        Iterator<Integer> iterator = numbers.iterator(); // один раз создаем iterator и затем с ним работаем
+        
+        while(iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        */
+        
         String finalText = String.format("Final list %s", numbers);
         System.out.println(finalText);
     }
