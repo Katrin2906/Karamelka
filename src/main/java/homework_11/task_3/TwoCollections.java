55faffb755172c0f4b59fa877f8ed7d60d40bdc0
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-12 07:48:03 +0200

    Update TwoCollections.java

--- src/main/java/homework_11/task_3/TwoCollections.java
+++ src/main/java/homework_11/task_3/TwoCollections.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class TwoCollections {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(15);
+        List<Integer> nums = new ArrayList<>(15); // 15 вынеси в отдельную переменную, например, Integer length = 15; и в цикле ниже тоже ее используй
         for (int i = 0; i < 15; i++) {
             nums.add(i);
         }
         System.out.println("First collection " + nums);
-        List<Integer> otherNums = new ArrayList<>();
+        List<Integer> otherNums = new ArrayList<>(); // здесь тоже можно было в конструктор передать 12
         for (int i = 5; i < 12; i++) {
             otherNums.add(i);
         }
