69e00ad9e8a5a6371122d1ad98f859e467de0380
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-14 17:18:13 +0200

    Update Multiplication.java

--- src/main/java/homework_12/task_2/Multiplication.java
+++ src/main/java/homework_12/task_2/Multiplication.java
@@ -2,6 +2,7 @@ package homework_12.task_2;
 
 import java.util.function.BinaryOperator;
 
+// +
 public class Multiplication {
     public static void main(String[] args) {
         BinaryOperator<Integer> multiply = (x, y) -> x * y;
