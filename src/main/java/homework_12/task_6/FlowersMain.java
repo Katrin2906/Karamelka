9d8ba5e3d75ada3f829d03067b7832079dee49dc
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-15 09:31:03 +0200

    Update FlowersMain.java

--- src/main/java/homework_12/task_6/FlowersMain.java
+++ src/main/java/homework_12/task_6/FlowersMain.java
@@ -6,14 +6,26 @@ import java.util.List;
 
 public class FlowersMain {
     public static void main(String[] args) {
-        List<String> flowers = new ArrayList<>();
+        List<String> flowers = new ArrayList<>(); // а зачем тогда класс Flowers?
         flowers.add("Rose");
         flowers.add("Daisy");
         flowers.add("Poppy");
         System.out.println(flowers);
-        flowers.forEach(fl -> System.out.println(Flowers.getName()));
+        flowers.forEach(fl -> System.out.println(Flowers.getName())); // есть подозрение что эта строка не компилируется, лучше сначала решить вопрос, а потом писать дальше
 
         var sortedFlowers = flowers.stream().sorted(Comparator.comparing(Flowers::getName, Comparator.reverseOrder())).toList();
 
+        /*
+        Вариант решения 1:
+        List<Flowers> flowers = new ArrayList<>();
+        Comparator<Flowers> flowersComparator = Comparator.comparing(Flowers::getName).reversed();
+        flowers.sort(flowersComparator);
+        */
+        
+        /*
+        Вариант решения 2:
+        List<String> flowers = new ArrayList<>();
+        flowers.sort(Comparator.reverseOrder());
+        */
     }
 }
