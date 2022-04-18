26cde57b9ff8c44b17f2c3566d809f9586a7ed67
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-18 21:10:21 +0200

    Update MobilePrce.java

--- src/main/java/homework_13/task_7/MobilePrce.java
+++ src/main/java/homework_13/task_7/MobilePrce.java
@@ -1,4 +1,4 @@
 package homework_13.task_7;
 
-public record MobilePrce(String producer, String model, Integer price) {
+public record MobilePrce(String producer, String model, Integer price) { // MobilePrce -> MobilePrice, будь внимательнее к названию классов и переменных
 }
