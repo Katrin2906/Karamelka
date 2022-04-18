1751f4f950f2bd44a27f04d8478cf9d96f24afe6
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-18 17:29:03 +0200

    Update FiveStrings.java

--- src/main/java/homework_13/task_1/FiveStrings.java
+++ src/main/java/homework_13/task_1/FiveStrings.java
@@ -6,16 +6,18 @@ import java.util.stream.Collectors;
 
 public class FiveStrings {
     public static void main(String[] args) {
-        List<String> badBuisness = new ArrayList<>(5);
+        List<String> badBuisness = new ArrayList<>(5); // badBuisness -> badBusiness
         badBuisness.add("Cassio");
         badBuisness.add("Omega");
         badBuisness.add("Breguet");
         badBuisness.add("Rolex");
         badBuisness.add("Piaget");
+        
         System.out.println("Initial collection " + badBuisness);
 
         List<String> shortWord = badBuisness.stream()
-                .filter(wordLength -> wordLength.length() >= 5).toList();
+                .filter(wordLength -> wordLength.length() >= 5).toList(); // toList() с новой строки
+       
         System.out.println("Sorted collection " + shortWord);
     }
 }
