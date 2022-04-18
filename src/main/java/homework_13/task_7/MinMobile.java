d2edd885ec4948c17a477496a75bcdc6fde34223
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-18 21:12:15 +0200

    Update MinMobile.java

--- src/main/java/homework_13/task_7/MinMobile.java
+++ src/main/java/homework_13/task_7/MinMobile.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-public class MinMobile {
+public class MinMobile { // MinMobile -> MinMobilePrice
     public static void main(String[] args) {
         List<MobilePrce> mobilePrces = new ArrayList<>();
         mobilePrces.add(new MobilePrce("Apple", "13 mini", 150));
@@ -15,9 +15,12 @@ public class MinMobile {
         mobilePrces.add(new MobilePrce("Apple", "13 mini", 100));
         mobilePrces.add(new MobilePrce("Xiaomi", "Note 11", 180));
         mobilePrces.add(new MobilePrce("BlackBerry", "Bold 9000", 230));
+        
         System.out.println(mobilePrces);
-        Optional<MobilePrce> minPrice = mobilePrces.stream()
+        
+        Optional<MobilePrce> minPrice = mobilePrces.stream() // minPrice - у тебя в переменной не минимальная цена, а телефон с минимальной ценой, mobileWithLowestPrice, например
                 .min(Comparator.comparing(MobilePrce::price));
+        
         System.out.println(minPrice);
     }
 }
