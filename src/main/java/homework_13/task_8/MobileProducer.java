d43738a51f991a54ead628607559fe16b5f3dc90
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-18 21:12:58 +0200

    Update MobileProducer.java

--- src/main/java/homework_13/task_8/MobileProducer.java
+++ src/main/java/homework_13/task_8/MobileProducer.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
 
+//+
 public class MobileProducer {
     public static void main(String[] args) {
         List<MobilePrce> mobilePrces = new ArrayList<>();
@@ -17,9 +18,12 @@ public class MobileProducer {
         mobilePrces.add(new MobilePrce("Apple", "13 mini", 100));
         mobilePrces.add(new MobilePrce("Xiaomi", "Note 11", 180));
         mobilePrces.add(new MobilePrce("BlackBerry", "Bold 9000", 230));
+        
         System.out.println(mobilePrces);
+        
         Map<String, List<MobilePrce>> findProducer = mobilePrces.stream()
                 .collect(Collectors.groupingBy(MobilePrce::producer));
+        
         System.out.println(findProducer);
     }
 }
