10536263e603d5b7dbf2c4fbf4a40376ddf81a7e
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-18 21:05:13 +0200

    Update FindNum.java

--- src/main/java/homework_13/task_4/FindNum.java
+++ src/main/java/homework_13/task_4/FindNum.java
@@ -11,8 +11,11 @@ public class FindNum {
         nums.add(0);
         nums.add(-56);
         nums.add(124);
-        nums.add(12);
+        nums.add(12); 
+        // можно разделять код абзацами, чтобы было удобнее выделять логические блоки
+        
         System.out.println(nums);
+        
         Optional<Integer> anyNum = nums.stream()
                 .filter(numsNew -> numsNew > 22)
                 .findFirst();
